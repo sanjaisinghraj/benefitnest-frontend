ef106e5ff0245aa7921aff9018a1edf550822a8d
sanjaisinghraj <sanjairajsingh1@gmail.com>
2025-12-25 15:32:38 +0530

    changed page.tsx file

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -11,7 +11,7 @@ const AdminLogin = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
 
-  const handleLogin = async (e) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
     setError('');
