744bf2c1946fee18eba2fa442763d3ab5e889232
sanjaisinghraj <sanjairajsingh1@gmail.com>
2025-12-25 12:16:52 +0530

    fix corporates page import path

--- app/admin/corporates/page.jsx
+++ app/admin/corporates/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import CorporatesManagement from '@/CorporatesManagement';
+import CorporatesManagement from '../../../CorporatesManagement';
 
 export default function CorporatesPage() {
   return <CorporatesManagement />;
