add1277bf2c373f6c3e68fc00d4a4dc2cebaa1ff
sanjaisinghraj <sanjairajsingh1@gmail.com>
2025-12-30 09:54:28 +0530

    all changes

--- proxy.ts
+++ proxy.ts
@@ -1,1 +1,17 @@
-export { proxy as default, config } from './app-proxy'
+import { proxy } from './app-proxy';
+
+// Configure which paths proxy runs on
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder files
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\..*|api/).*)",
+  ],
+};
+
+export default proxy;
